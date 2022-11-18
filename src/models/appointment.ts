31297f19af431c2f9ffb17cf848beea1297afda4
connylu <gusluoco@student.gu.se>
2022-11-18 09:27:19 +0100

    Add appointment schema

---
+++ src/models/appointment.ts
@@ -0,0 +1,17 @@
+/* userid:  String, requestid: String , dentistid: String , issuance: boolean, date: Date, price: float, treatment: String/Enum */
+import mongoose, { Schema } from 'mongoose'
+
+const appointmentSchema = new Schema({
+  user_id: String,
+  request_id: String,
+  dentist_id: String,
+  issuance: Boolean,
+  date: Date,
+  price: Number,
+  treatment: { 
+    type: String,
+    enum: ["null"]
+  }
+})
+
+module.exports = mongoose.model('appointment', appointmentSchema)
