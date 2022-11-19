b523621753d9b1c5cbda796edc2dd175cd5dbeb6
connylu <gusluoco@student.gu.se>
2022-11-19 19:51:39 +0100

    Add find method for appointment controller

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,13 +1,29 @@
 import { Request, Response, NextFunction } from 'express'
-import Appointment from '../models/appointment'
+import { CallbackError } from 'mongoose'
+import Appointment, { Appointment as AppointmentType } from '../models/appointment'
 
 exports.create = (req: Request, res: Response, next: NextFunction) => {
   const appointment = new Appointment(req.body)
-  appointment.save((err, appointment) => {
+  appointment.save((err: CallbackError, appointment: AppointmentType) => {
     if (err) {
       return next(err)
     }
     return res.status(201).json(appointment)
+  })
+}
+
+exports.findOne = (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id
+  Appointment.findById(id, (err: CallbackError, appointment: AppointmentType) => {
+    if (err) {
+      return next(err)
+    }
+    
+    if (!appointment) {
+      return res.status(404).json({'message': "Appointment not found."})
+    }
+
+    return res.status(200).json(appointment)
   })
 }
 
