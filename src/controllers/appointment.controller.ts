39a7b5ab4bb16d07d82ca379a77a1f2ffdbbfc91
connylu <gusluoco@student.gu.se>
2022-11-21 16:15:59 +0100

    Update appointment controller

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from 'express'
 import { CallbackError } from 'mongoose'
-import Appointment, { Appointment as AppointmentType } from '../models/appointment'
+import Appointment, { Appointment as TAppointment } from '../types/appointment'
 
 exports.create = (req: Request, res: Response, next: NextFunction) => {
   const appointment = new Appointment(req.body)
-  appointment.save((err: CallbackError, appointment: AppointmentType) => {
+  appointment.save((err: CallbackError, appointment: TAppointment) => {
     if (err) {
       return next(err)
     }
@@ -14,7 +14,7 @@ exports.create = (req: Request, res: Response, next: NextFunction) => {
 
 exports.findOne = (req: Request, res: Response, next: NextFunction) => {
   const id = req.params.id
-  Appointment.findById(id, (err: CallbackError, appointment: AppointmentType) => {
+  Appointment.findById(id, (err: CallbackError, appointment: TAppointment) => {
     if (err) {
       return next(err)
     }
@@ -27,10 +27,32 @@ exports.findOne = (req: Request, res: Response, next: NextFunction) => {
   })
 }
 
-exports.updateDate = () => {
-  return
+exports.updateOne = (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id
+  Appointment.findByIdAndUpdate(id, req.body, (err: CallbackError, appointment: TAppointment) => {
+    if (err) {
+      return next(err)
+    }
+    
+    if (!appointment) {
+      return res.status(404).json({'message': "Appointment not found."})
+    }
+
+    return res.status(200).json(appointment)
+  })
 }
 
-exports.archive = () => {
-  return
+exports.deleteOne = (req: Request, res: Response, next: NextFunction) => {
+  const id = req.params.id
+  Appointment.findByIdAndDelete(id, (err: CallbackError, appointment: TAppointment) => {
+    if (err) {
+      return next(err)
+    }
+    
+    if (!appointment) {
+      return res.status(404).json({'message': "Appointment not found."})
+    }
+
+    return res.status(200).json(appointment)
+  })
 }
