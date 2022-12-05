eacffea0941dcc4ce22628d1bca163cd3afea79c
connylu <gusluoco@student.gu.se>
2022-12-05 12:18:41 +0100

    Add preliminary test details for appointment

---
+++ tests/appointment/appointment.test.ts
@@ -0,0 +1,35 @@
+import { assert } from 'chai'
+
+describe('Appointment Creation', () => {
+  describe('createAppointment(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  }),
+  describe('createAppointment(), with invalid parameters', () => {
+    it('should X', () => {
+      // TODO: Assert
+      return false
+    }),
+    it('should Y', () => {
+      // TODO: Assert
+      return false
+    })
+  }),
+  describe('createAppointment(), with valid parameters', () => {
+    it('should X', () => {
+      // TODO: Assert
+      return false
+    })
+  })
+
+  /* TODO:
+    - Test getAppointmentsFromUserId()
+    - Test getUpcomingAppointmentsFromUserId()
+    - Test getAllAppointments()
+    - Test getAppointmentHistoryFromUserId()
+    - Test updateAppointmentTime()
+  */
+
+})
