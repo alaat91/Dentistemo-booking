3afeff49925b58d93a037939732627ccc945e722
connylu <gusluoco@student.gu.se>
2022-12-07 21:33:12 +0100

    Complete more placeholder tests

--- tests/appointment/appointment.test.ts
+++ tests/appointment/appointment.test.ts
@@ -1,6 +1,10 @@
-import { assert } from 'chai'
+// import { assert } from 'chai'
 
-describe('Appointment Creation', () => {
+/*
+* This section of unit tests is for creating appointments.
+* Tests should attempt use invalid vs. valid parameters, expect former to error and latter to return objects.
+*/
+describe('Create appointments for user', () => {
   describe('createAppointment(), no parameters', () => {
     it('should return an error on', () => {
       // TODO: Assert
@@ -23,13 +27,64 @@ describe('Appointment Creation', () => {
       return false
     })
   })
+})
 
-  /* TODO:
-    - Test getAppointmentsFromUserId()
-    - Test getUpcomingAppointmentsFromUserId()
-    - Test getAllAppointments()
-    - Test getAppointmentHistoryFromUserId()
-    - Test updateAppointmentTime()
-  */
+/*
+* This section of unit tests is for getting appointments from one user's ID.
+*/
+describe('Getting appointments from one user', () => {
+  describe('getAppointmentsFromUserId(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  })
+})
 
+/*
+* This section of unit tests is for getting appointments from one user's ID.
+*/
+describe('Getting upcoming appointments from one user', () => {
+  describe('getUpcomingAppointmentsFromUserId(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  })
+})
+
+/*
+* This section of unit tests is for getting historic appointments from one user's ID.
+*/
+describe('Getting all historic appointments from one user', () => {
+  describe('getAppointmentHistoryFromUserId(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  })
+})
+
+/*
+* This section of unit tests is for getting all appointments.
+*/
+describe('Getting all booked appointments', () => {
+  describe('getAllAppointments(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  })
+})
+
+/*
+* This section of unit tests is for updating appointments.
+*/
+describe('Updating appointment time on existing appointment', () => {
+  describe('updateAppointmentTime(), no parameters', () => {
+    it('should return an error on', () => {
+      // TODO: Assert
+      return false
+    })
+  })
 })
