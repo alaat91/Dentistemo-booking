ed99afb7dfc65a8f9a6763a714c4bbc491e06363
connylu <gusluoco@student.gu.se>
2022-12-14 10:30:51 +0100

    Add method documentation for timeslots

--- src/controllers/timeslot.ts
+++ src/controllers/timeslot.ts
@@ -1,8 +1,13 @@
 import { ReadonlyITimeslot } from "../interfaces/timeslot"
 
 /**
-* Creates a timeslot object.
-*/
+ * 
+ * Creates a timeslot object.
+ * @param {Date} startDate Starting date of timeslot.
+ * @param {Date} endDate Ending date of timeslot.
+ * @returns {ReadonlyITimeslot} Timeslot object containing start and end date.
+ *
+ */
 async function createTimeSlot(startDate: Date, endDate: Date): Promise<ReadonlyITimeslot> {
   return {
     start_date: startDate,
@@ -11,15 +16,31 @@ async function createTimeSlot(startDate: Date, endDate: Date): Promise<ReadonlyI
 }
 
 /**
-* Returns the start date of timeslot.
-*/
+ * 
+ * @param timeslot 
+ * @returns 
+ * 
+ */
+
+
+/**
+ * 
+ * Returns the start date of timeslot.
+ * @param {ReadonlyITimeslot} timeslot Timeslot object.
+ * @returns {Date} Starting date of timeslot.
+ * 
+ */
 async function getStartDate(timeslot: ReadonlyITimeslot): Promise<Date> {
   return timeslot.start_date
 }
 
 /**
-* Returns the end date of timeslot.
-*/
+ * 
+ * Returns the end date of timeslot.
+ * @param {ReadonlyITimeslot} timeslot Timeslot object.
+ * @returns {Date} Ending date of timeslot.
+ * 
+ */
 async function getEndDate(timeslot: ReadonlyITimeslot): Promise<Date> {
   return timeslot.end_date
 }
