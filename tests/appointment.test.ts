b66f6362c4fcead055d9d3cb63a4eb26de5eb45e
connylu <gusluoco@student.gu.se>
2022-12-20 11:25:22 +0100

    Update test file for integrating the model

--- tests/appointment.test.ts
+++ tests/appointment.test.ts
@@ -1,29 +1,51 @@
 import { assert } from 'chai'
-import dropDB from './util/dropdb'
+import mongoose from 'mongoose'
+import appointment from '../src/controllers/appointment'
 
-require('../src/app')
+const mongoURI: string = process.env.MONGODB_URI as string || 'mongodb://localhost:27017/dentistimo'
+
+before(() => {
+  mongoose.connect(mongoURI, (err) => {
+    if (err) {
+      // Connection failure here
+      process.exit(1)
+    }
+    // Connection successful
+  })
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
 
 /*
 * This section of unit tests is for creating appointments.
 * Tests should attempt use invalid vs. valid parameters, expect former to error and latter to return objects.
 */
-describe('Create appointments for user', () => {
+describe('Create appointments for user', async () => {
+  after(async () => {
+    mongoose.connection.dropDatabase()
+  })
   describe('createAppointment(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      appointment.createAppointment('').then((result) => {
+        assert.equal(result, null, 'Appointment generated something')
+      }).catch((err) => {
+        assert.equal(err, '', 'Appointment did not generate error')
+      })
     })
-  }),
+  })
   describe('createAppointment(), with invalid parameters', () => {
     it('should X', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     }),
     it('should Y', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
-  }),
+  })
   describe('createAppointment(), with valid parameters', () => {
     it('should X', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
@@ -31,10 +53,10 @@ describe('Create appointments for user', () => {
 /*
 * This section of unit tests is for getting appointments from one user's ID.
 */
-describe('Getting appointments from one user', () => {
+describe('Getting appointments from one user', async () => {
   describe('getAppointmentsFromUserId(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
@@ -42,10 +64,10 @@ describe('Getting appointments from one user', () => {
 /*
 * This section of unit tests is for getting appointments from one user's ID.
 */
-describe('Getting upcoming appointments from one user', () => {
+describe('Getting upcoming appointments from one user', async () => {
   describe('getUpcomingAppointmentsFromUserId(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
@@ -53,10 +75,10 @@ describe('Getting upcoming appointments from one user', () => {
 /*
 * This section of unit tests is for getting historic appointments from one user's ID.
 */
-describe('Getting all historic appointments from one user', () => {
+describe('Getting all historic appointments from one user', async () => {
   describe('getAppointmentHistoryFromUserId(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
@@ -64,10 +86,10 @@ describe('Getting all historic appointments from one user', () => {
 /*
 * This section of unit tests is for getting all appointments.
 */
-describe('Getting all booked appointments', () => {
+describe('Getting all booked appointments', async () => {
   describe('getAllAppointments(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
@@ -75,12 +97,10 @@ describe('Getting all booked appointments', () => {
 /*
 * This section of unit tests is for updating appointments.
 */
-describe('Updating appointment time on existing appointment', () => {
+describe('Updating appointment time on existing appointment', async () => {
   describe('updateAppointmentTime(), no parameters', () => {
     it('should return an error on', () => {
-      assert.isTrue(true, "Placeholder failed!?")
+      assert.isTrue(true, 'Placeholder failed!?')
     })
   })
 })
-
-dropDB()
